10f24fe1c61f4fd6893cdcb48785fe9d4947c012
Виктор Бугай <ascon2913@gmail.com>
2024-06-12 21:19:01 +0500

    homework 10.2 done

---
+++ tests/test_processing.py
@@ -0,0 +1,97 @@
+from src.processing import filter_by_state, sort_by_date
+import pytest
+
+
+# демонстрация тестов для filter_by_state с помощью фикстур
+def test_filter_by_state_ex(filter_ex):
+    assert (
+        filter_by_state(
+            [
+                {"id": 41428829, "state": "EXECUTED", "date": "2019-07-03T18:35:29.512364"},
+                {"id": 939719570, "state": "EXECUTED", "date": "2018-06-30T02:08:58.425572"},
+                {"id": 594226727, "state": "CANCELED", "date": "2018-09-12T21:27:25.241689"},
+                {"id": 615064591, "state": "CANCELED", "date": "2018-10-14T08:21:33.419441"},
+            ]
+        )
+        == filter_ex
+    )
+
+
+def test_filter_by_state_canceled(filter_canceled):
+    assert (
+        filter_by_state(
+            [
+                {"id": 41428829, "state": "EXECUTED", "date": "2019-07-03T18:35:29.512364"},
+                {"id": 939719570, "state": "EXECUTED", "date": "2018-06-30T02:08:58.425572"},
+                {"id": 594226727, "state": "CANCELED", "date": "2018-09-12T21:27:25.241689"},
+                {"id": 615064591, "state": "CANCELED", "date": "2018-10-14T08:21:33.419441"},
+            ],
+            state="CANCELED",
+        )
+        == filter_canceled
+    )
+
+
+# демонстрация тестов для filter_by_state с помощью параметризации
+@pytest.mark.parametrize(
+    "data, state, expected",
+    [
+        (
+            [
+                {"id": 41428829, "state": "EXECUTED", "date": "2019-07-03T18:35:29.512364"},
+                {"id": 939719570, "state": "EXECUTED", "date": "2018-06-30T02:08:58.425572"},
+                {"id": 594226727, "state": "CANCELED", "date": "2018-09-12T21:27:25.241689"},
+                {"id": 615064591, "state": "CANCELED", "date": "2018-10-14T08:21:33.419441"},
+            ],
+            "CANCELED",
+            [
+                {"id": 594226727, "state": "CANCELED", "date": "2018-09-12T21:27:25.241689"},
+                {"id": 615064591, "state": "CANCELED", "date": "2018-10-14T08:21:33.419441"},
+            ],
+        ),
+        (
+            [
+                {"id": 41428829, "state": "EXECUTED", "date": "2019-07-03T18:35:29.512364"},
+                {"id": 939719570, "state": "EXECUTED", "date": "2018-06-30T02:08:58.425572"},
+                {"id": 594226727, "state": "CANCELED", "date": "2018-09-12T21:27:25.241689"},
+                {"id": 615064591, "state": "CANCELED", "date": "2018-10-14T08:21:33.419441"},
+            ],
+            "EXECUTED",
+            [
+                {"id": 41428829, "state": "EXECUTED", "date": "2019-07-03T18:35:29.512364"},
+                {"id": 939719570, "state": "EXECUTED", "date": "2018-06-30T02:08:58.425572"},
+            ],
+        ),
+    ],
+)
+def test_filter_by_state(data, state, expected):
+    assert filter_by_state(data, state) == expected
+
+
+def test_sort_by_date(sort_base):
+    assert (
+        sort_by_date(
+            [
+                {"id": 41428829, "state": "EXECUTED", "date": "2019-07-03T18:35:29.512364"},
+                {"id": 939719570, "state": "EXECUTED", "date": "2018-06-30T02:08:58.425572"},
+                {"id": 594226727, "state": "CANCELED", "date": "2018-09-12T21:27:25.241689"},
+                {"id": 615064591, "state": "CANCELED", "date": "2018-10-14T08:21:33.419441"},
+            ]
+        )
+        == sort_base
+    )
+
+
+def test_sort_by_date_reverse(sort_reverse):
+    assert (
+        sort_by_date(
+            [
+                {"id": 41428829, "state": "EXECUTED", "date": "2019-07-03T18:35:29.512364"},
+                {"id": 939719570, "state": "EXECUTED", "date": "2018-06-30T02:08:58.425572"},
+                {"id": 594226727, "state": "CANCELED", "date": "2018-09-12T21:27:25.241689"},
+                {"id": 615064591, "state": "CANCELED", "date": "2018-10-14T08:21:33.419441"},
+            ],
+            rev=False,
+        )
+        == sort_reverse
+    )
---
+++ tests/test_widget.py
@@ -0,0 +1,29 @@
+from src.widget import mask_account_card, get_data
+import pytest
+
+
+@pytest.mark.parametrize(
+    "value, expected",
+    [
+        ("Счет 64686473678894779589", "Счет **9589"),
+        ("Maestro 1596837868705199", "Maestro 1596 83** **** 5199"),
+        ("MasterCard 7158300734726758", "MasterCard 7158 30** **** 6758"),
+        ("Visa Classic 6831982476737658", "Visa Classic 6831 98** **** 7658"),
+        ("Visa Platinum 8990922113665229", "Visa Platinum 8990 92** **** 5229"),
+        ("Visa Gold 5999414228426353", "Visa Gold 5999 41** **** 6353"),
+    ],
+)
+def test_mask_account_card(value, expected):
+    assert mask_account_card(value) == expected
+
+
+@pytest.mark.parametrize(
+    "value, expected",
+    [
+        ("2018-07-11T02:26:18.671407", "11.07.2018"),
+        ("2019-07-03T18:35:29.512364", "03.07.2019"),
+        ("2077-01-29T20:07:07.926541", "29.01.2077"),
+    ],
+)
+def test_get_data(value, expected):
+    assert get_data(value) == expected
