7ec95fb30ac6f68fa51efd39fc37c5c6bb6322c4
Ascon29 <ascon2913@gmail.com>
2024-07-01 16:33:47 +0500

    homework 12.2 done

--- src/utils.py
+++ src/utils.py
@@ -1,5 +1,13 @@
 import json
+import logging
 import os
+
+logger = logging.getLogger("utils")
+logger.setLevel(logging.INFO)
+file_handler = logging.FileHandler("logs/utils.log", "w")
+file_formatter = logging.Formatter("%(asctime)s - %(name)s - %(levelname)s: %(message)s")
+file_handler.setFormatter(file_formatter)
+logger.addHandler(file_handler)
 
 
 def get_operations(file_path):
@@ -7,14 +15,20 @@ def get_operations(file_path):
     и возвращает список словарей с данными о финансовых транзакциях.
     Если файл пустой, содержит не список или не найден,
     функция возвращает пустой список."""
+    logger.info(f"попытка открыть файл {file_path}")
     if not os.path.exists(file_path):
+        logger.warning(f"файл {file_path} отсуствует")
         return []
     try:
+        logger.info(f"открытие файла {file_path}")
         with open(file_path, encoding="utf-8") as file:
             operations = json.load(file)
+            logger.info(f"запись информации {operations}")
             if isinstance(operations, list):
                 return operations
             else:
+                logger.warning(f"файл {file_path} содержит не список(list")
                 return []
     except json.JSONDecodeError:
+        logger.error(f"Ошибка {json.JSONDecodeError}")
         return []
