a287d73801e7270f14305c765a0d1fb61467631c
Ascon29 <ascon2913@gmail.com>
2024-07-04 16:06:15 +0500

    homework 13.1 done

--- src/utils.py
+++ src/utils.py
@@ -1,6 +1,8 @@
 import json
 import logging
 import os
+import pandas as pd
+import csv
 
 logger = logging.getLogger("utils")
 logger.setLevel(logging.INFO)
@@ -36,3 +38,34 @@ def get_operations(file_path):
     except json.JSONDecodeError:
         logger.error(f"Ошибка {json.JSONDecodeError}")
         return []
+
+
+def get_operations_from_csv(file_path):
+    '''функция принимает путь до CSV файла и возвращает список с данными
+    о финансовых транзакциях
+    :param file_path: путь до CSV файла.
+    :return: list с данными о транзакцих
+    '''
+    with open(file_path, "r", encoding="utf-8") as file:
+        reader = csv.reader(file, delimiter=";")
+        header = next(reader)
+        result = []
+        for row in reader:
+            row_dict = dict()
+            for index, item in enumerate(header):
+                row_dict[item] = row[index]
+            result.append(row_dict)
+    return result
+
+
+def get_operations_from_xlsx(file_path):
+    '''функция принимает путь до XLSX файла и возвращает список с данными
+    о финансовых транзакциях
+    :param file_path: путь до XLSX файла.
+    :return: list с данными о транзакцих
+    '''
+    operations = pd.read_excel(file_path).to_json(orient='records')
+    return json.loads(operations)
+
+# print(get_operations_from_csv('data/transactions.csv'))
+# print(get_operations_from_xlsx('data/transactions_excel.xlsx'))
